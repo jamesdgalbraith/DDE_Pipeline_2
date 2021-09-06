51cb91aab9b57a296390df1317691967ba14f8da
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-06 17:15:38 +0100

    Created script for GNU-parallel tblastn

---
+++ first_search.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Count contigs in genome
+CONTIGS="`grep -c '>' seq/${GENOME}.fasta`"
+
+# Search in parallel
+parallel --bar -j0 -a families.txt tblastn -query data/representative/{}.fasta -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid length qstart qend qlen sstart send slen evalue\" -out out/{.}_in_${GENOME}.out -max_target_seqs ${CONTIGS}
