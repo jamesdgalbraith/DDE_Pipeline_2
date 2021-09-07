d4cbb557bb5081501bd2e3e86ee8e39450f338ee
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-07 17:56:32 +0100

    added script to remove outgroups

--- remove_outgroup.R
+++ remove_outgroup.R
@@ -8,30 +8,26 @@ families <- getSheetNames("data/Supp-info_clades_CP_updt.xlsx")
 
 # For each family
 for(i in 1:length(families)){
-
+  
   # Create compilation
   if(i == 1){compiled_seq <- AAStringSet()}
-    
+  
   # Read in sheet of family
   sheet1 <- as_tibble(read.xlsx(xlsxFile = "data/Supp-info_clades_CP_updt.xlsx", sheet = families[i], colNames = F))
   # Name columns
   colnames(sheet1) <- c("clade", "seqnames")
-  # select representatives
-  sheet1 <- sheet1[!is.na(sheet1$clade),]
   # Read in family sequence
   in_seq <- readAAStringSet(paste0("data/unaligned/", families[i], ".fasta"))
   
   # Select representative sequences
   filtered_seq <- in_seq[names(in_seq) %in% sheet1$seqnames]
-  # Add family name to sequence if not present
-  names(filtered_seq) <- paste0(families[i], "_", 1:length(filtered_seq))
   # Write representatives to file
-  writeXStringSet(filtered_seq, paste0("data/representative/", families[i], ".fasta"))
+  writeXStringSet(filtered_seq, paste0("data/unaligned/", families[i], ".fasta"))
   
   # Add representatives to collated sequences
   compiled_seq <- c(compiled_seq, filtered_seq)
   
-  if(i == length(families)){writeXStringSet(compiled_seq, "data/representative/compiled.fasta")}
+  if(i == length(families)){writeXStringSet(compiled_seq, "data/unaligned/compiled.fasta")}
   
 }
 
