cb10b55fcd170c9b7fa46d669585557689fb43b2
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-07 12:53:45 +0100

    Addded script to split multifastas

--- find_representative.R
+++ find_representative.R
@@ -8,24 +8,32 @@ families <- getSheetNames("data/Supp-info_clades_CP_updt.xlsx")
 
 # For each family
 for(i in 1:length(families)){
-  
+
+  # Create compilation
+  if(i == 1){compiled_seq <- AAStringSet()}
+    
   # Read in sheet of family
   sheet1 <- as_tibble(read.xlsx(xlsxFile = "data/Supp-info_clades_CP_updt.xlsx", sheet = families[i], colNames = F))
   # Name columns
   colnames(sheet1) <- c("clade", "seqnames")
   # select representatives
-  sheet1 <- sheet1[(!is.na(sheet1$clade) & sheet1$clade != "Outgroup"),]
-  
+  sheet1 <- sheet1[!is.na(sheet1$clade),]
   # Read in family sequence
   in_seq <- readAAStringSet(paste0("data/", families[i], ".fasta"))
-  # Select representaive sequences
+  
+  # Select representative sequences
   filtered_seq <- in_seq[names(in_seq) %in% sheet1$seqnames]
   # Add family name to sequence if not present
-  names(filtered_seq) <- ifelse(grepl(families[i], names(filtered_seq)),
-                                names(filtered_seq),
-                                paste0(families[i], "_", names(filtered_seq)))
-  
+  names(filtered_seq) <- paste0(families[i], "_", 1:length(filtered_seq))
   # Write representatives to file
   writeXStringSet(filtered_seq, paste0("data/representative/", families[i], ".fasta"))
+  
+  # Add representatives to collated sequences
+  compiled_seq <- c(compiled_seq, filtered_seq)
+  
+  tibble(names(filtered_seq), names(in_seq[names(in_seq) %in% sheet1$seqnames]))
 
+  if(i == length(families)){writeXStringSet(compiled_seq, "data/representative/compiled.fasta")}
+  
 }
+
--- first_search.sh
+++ first_search.sh
@@ -4,4 +4,4 @@
 CONTIGS="`grep -c '>' seq/${GENOME}.fasta`"
 
 # Search in parallel
-parallel --bar -j0 -a families.txt tblastn -query data/representative/{}.fasta -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid length qstart qend qlen sstart send slen evalue\" -out out/{.}_in_${GENOME}.out -max_target_seqs ${CONTIGS}
+parallel --bar --jobs 4 -a families.txt tblastn -query data/representative/{}.fasta -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid length qstart qend qlen sstart send slen evalue\" -out out/{.}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 2
