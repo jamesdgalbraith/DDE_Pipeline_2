8f5f1e3fb1083131b2a1f2a525fd798a3e6c814d
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-08 11:45:10 +0100

    Updated paths in first_search script

--- first_search.sh
+++ first_search.sh
@@ -4,4 +4,6 @@
 CONTIGS="`grep -c '>' seq/${GENOME}.fasta`"
 
 # Search in parallel
-parallel --bar --jobs 4 -a families.txt tblastn -query data/representative/{}.fasta -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid length qstart qend qlen sstart send slen evalue\" -out out/{.}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 2
+parallel --bar --jobs 4 -a data/queries.txt tblastn -query data/representative/search_queries/{}.fasta -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue\" -out out/split_out/{.}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 2
+
+cat out/split_out/*_in_${GENOME}.out > out/split_out/compiled_in_${GENOME}.out
