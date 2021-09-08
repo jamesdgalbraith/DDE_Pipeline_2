387794a9216bfcd4b76d23e58f4c168342b8c76b
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-08 12:33:58 +0100

    Added reciprocal search

---
+++ recip_search.R
@@ -0,0 +1,38 @@
+library(BSgenome)
+library(plyranges)
+library(tidyverse)
+
+# read in blast output
+tblastn_in <- read_tsv(paste0("out/tblastn/compiled_in_", "Glomeris_maerens", ".out"),
+                       col_names = c("qseqid", "seqnames", "pident", "length", "qstart", "qend", "qlen", "sstart", "send", "slen", "evalue"))
+
+# read in genome
+genome_seq <- readDNAStringSet(paste0("seq/", "Glomeris_maerens", ".fasta"))
+names(genome_seq) <- gsub(" .*", "", names(genome_seq))
+
+# filter blast output
+tblastn_fixed <- tblastn_in %>%
+  filter(length >= 0.5*qlen) %>%
+  mutate(strand = ifelse(sstart < send, "+", "-"),
+         start = ifelse(strand == "+", sstart, send),
+         end = ifelse(strand == "+", send, sstart)) %>%
+  as_granges()
+
+# get filtered seq
+tblastn_ranges <- GenomicRanges::reduce(tblastn_fixed)
+tblastn_seq <- getSeq(genome_seq, tblastn_ranges)
+names(tblastn_seq) <- paste0(seqnames(tblastn_ranges), ":", ranges(tblastn_ranges), "(", strand(tblastn_ranges), ")")
+
+writeXStringSet(tblastn_seq, paste0("out/plain_tblastn_initial_fastas/", "Glomeris_maerens", "_seq_nt.fasta"))
+writeXStringSet(x = translate(tblastn_seq, if.fuzzy.codon = "solve"),
+                filepath = paste0("out/plain_tblastn_initial_fastas/", "Glomeris_maerens", "_seq_aa.fasta"))
+
+recip_blast <- read_tsv(
+  system(
+    paste0("blastp -query out/plain_tblastn_initial_fastas/", "Glomeris_maerens",
+           "_seq_aa.fasta -db data/representative/compiled_database/compiled.fasta -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue\""),
+    intern = T),
+  col_names = c("qseqid", "seqnames", "pident", "length", "qstart", "qend", "qlen", "sstart", "send", "slen", "evalue")
+  )
+
+recip_blast
