3cf1e09d2d915ec443c375d8f590be28f6ee5ef4
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-14 12:23:28 +0100

    fixed query paths

--- first_search.sh
+++ first_search.sh
@@ -7,7 +7,7 @@ CONTIGS="`grep -c '>' seq/${GENOME}.fasta`"
 #makeblastdb -dbtype nucl -in seq/${GENOME}.fasta
 
 # Search in parallel
-parallel --bar --jobs ${THREADS} -a data/queries.txt tblastn -query data/representative/search_queries/{} -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue frames\" -out out/split_out/{}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 1
+parallel --bar --jobs ${THREADS} -a data/queries.txt tblastn -query data/search_queries/{} -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue frames\" -out out/split_out/{}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 1
 
 # combine blast output
 cat out/split_out/*_in_${GENOME}.out > out/tblastn/compiled_in_${GENOME}.out
--- pipeline_mk1.sh
+++ pipeline_mk1.sh
@@ -10,7 +10,7 @@ CONTIGS="`grep -c '>' seq/${GENOME}.fasta`"
 makeblastdb -dbtype nucl -in seq/${GENOME}.fasta
 
 # Search in parallel
-parallel --bar --jobs ${THREADS} -a data/queries.txt tblastn -query data/representative/search_queries/{} -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue frames\" -out out/split_out/{}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 1
+parallel --bar --jobs ${THREADS} -a data/queries.txt tblastn -query data/search_queries/{} -db seq/${GENOME}.fasta -evalue 1e-5 -outfmt \"6 qseqid sseqid pident length qstart qend qlen sstart send slen evalue frames\" -out out/split_out/{}_in_${GENOME}.out -max_target_seqs ${CONTIGS} -num_threads 1
 
 # combine blast output
 cat out/split_out/*_in_${GENOME}.out > out/tblastn/compiled_in_${GENOME}.out
@@ -25,7 +25,7 @@ sed -i 's/\*/X/g' out/plain_tblastn_initial_fastas/${GENOME}_seq.fasta
 mkdir -p out/recip/
 
 # blast against original query
-blastp -num_threads ${THREADS} -query out/plain_tblastn_initial_fastas/${GENOME}_seq.fasta -db data/representative/compiled_database/compiled.fasta -outfmt "6 qseqid sseqid pident length qstart qend qlen sstart send slen" -out out/recip/${GENOME}.out
+blastp -num_threads ${THREADS} -query out/plain_tblastn_initial_fastas/${GENOME}_seq.fasta -db data/compiled_database/compiled.fasta -outfmt "6 qseqid sseqid pident length qstart qend qlen sstart send slen" -out out/recip/${GENOME}.out
 
 # classify repeats
 Rscript recip_search.R -s ${GENOME}
