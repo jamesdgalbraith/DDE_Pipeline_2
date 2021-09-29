c7cdbe5d532d4c03c1b92fbf0e04524420f9501b
jamesdgalbraith <galbraithenator@gmail.com>
2021-09-29 15:08:55 +0100

    Major corrections allow custom flank lengths

--- orf_finder.R
+++ orf_finder.R
@@ -2,18 +2,16 @@ suppressPackageStartupMessages({
   library(optparse)
 })
 
-# genome_name = "latCor_2.0"
-# species_name = "Laticauda_colubrina"
-# outdir <- "out/plain_tblastn_initial_fastas/"
-
 # parse input variables
 option_list = list(
   make_option(c("-g", "--genome_name"), type="character", default=NULL, 
               help="genome name", metavar="character"),
   make_option(c("-s", "--species_name"), type="character", default=NULL,
               help="genome name", metavar="character"),
-  make_option(c("-o", "--out"), type="character", default="out/plain_tblastn_initial_fastas/", 
-              help="path to output [default= %default]", metavar="character")
+  make_option(c("-o", "--out"), type="character", default="out/orfs/", 
+              help="path to output [default= %default]", metavar="character"),
+  make_option(c("-f", "--flank_size"), type="integer", default=450, 
+              help="Size of flanking sequence in nt (must be divisible by 3)")
   
 )
 
@@ -34,6 +32,26 @@ if (is.null(opt$species_name)) {
   species_name <- opt$species_name
 }
 
+message(species_name)
+
+if(opt$flank_size%%3 != 0){
+  stop("Size of flanking sequence in nt must be divisible by 3")
+} else {
+  flank <- opt$flank_size
+}
+
+if(!dir.exists(paste0(outdir, "/aa"))){
+  dir.create(paste0(outdir, "/aa"))
+}
+
+if(!dir.exists(paste0(outdir, "/nt"))){
+  dir.create(paste0(outdir, "/nt"))
+}
+
+# genome_name = "latCor_2.0"
+# species_name = "Laticauda_colubrina"
+# outdir <- "out/orfs/"
+
 suppressPackageStartupMessages({
   library(BSgenome)
   library(plyranges)
@@ -41,7 +59,7 @@ suppressPackageStartupMessages({
   library(ORFik)
 })
 
-if(!file.exists(paste0(outdir, "/", genome_name, "_single_frame.bed"))){
+if(!file.exists(paste0("out/plain_tblastn_initial_fastas/", genome_name, "_single_frame.bed"))){
   stop("BED contains location of transposes identified in a single frame needed")
 }
 
@@ -50,17 +68,24 @@ genome_seq <- readDNAStringSet(paste0("seq/", genome_name, ".fasta"))
 names(genome_seq) <- sub(" .*", "", names(genome_seq))
 
 # read in bed as tibble (+1 to correct start)
-single_frame_bed <- read_tsv(paste0("out/plain_tblastn_initial_fastas/", species_name, "_single_frame.bed"),
-                             col_names = c("seqnames", "start", "end", "name", "score", "strand")) %>%
-  mutate(start = start + 1, name = paste0(seqnames, ":", start, "-", end, "(", strand, ")"),
-         frame = case_when(start%%3 == 1 ~ 1, start%%3 == 2 ~ 2, start%%3 == 0 ~ 3)) %>%
-  inner_join(tibble(seqnames = names(genome_seq), contig_width = width(genome_seq)))
+suppressMessages(
+  single_frame_bed <- read_tsv(paste0("out/plain_tblastn_initial_fastas/", genome_name, "_single_frame.bed"),
+                               col_names = c("seqnames", "start", "end", "name", "score", "strand")) %>%
+    mutate(start = start + 1, name = paste0(seqnames, ":", start, "-", end, "(", strand, ")"),
+           frame = case_when(start%%3 == 1 ~ 1, start%%3 == 2 ~ 2, start%%3 == 0 ~ 3)) %>%
+    inner_join(tibble(seqnames = names(genome_seq), contig_width = width(genome_seq)))
+  )
 
 # read in classification of all, join to keep only singlke frames
-single_frame_classified <- tibble(name = names(readAAStringSet(paste0("out/classified_tnps/", species_name, "_compiled.fasta")))) %>%
-  separate(name, into = c("name", "class"), sep = "#") %>%
-  inner_join(single_frame_bed) %>%
-  mutate(start = start - 450, end = end + 450, width = end - start +1) %>%
+suppressMessages(
+  single_frame_classified <- tibble(name = names(readAAStringSet(paste0("out/classified_tnps/", species_name, "_compiled.fasta")))) %>%
+    separate(name, into = c("name", "class"), sep = "#") %>%
+    inner_join(single_frame_bed)
+  )
+
+# extend flanks by multiple of 3
+single_frame_classified <- single_frame_classified %>%
+  mutate(start = start - flank, end = end + flank, width = end - start +1) %>%
   filter(start > 1, end <= contig_width)
 
 # convert classified to ranges
@@ -88,45 +113,49 @@ classified_seq_tibble <- tibble(name = names(single_frame_classified_seq)[as.int
   mutate(strand = sub(")", "", strand), start = as.integer(start), end = as.integer(end))
 
 # Ensure orfs begin before and end after domain
-single_frame_classified_orfs <- single_frame_classified %>%
-  inner_join(classified_seq_tibble) %>%
-  filter(orf_start <= 450) %>%
-  filter(orf_end > width - 450) %>%
-  arrange(orf_end - orf_start)
+suppressMessages(
+  single_frame_classified_orfs <- single_frame_classified %>%
+    dplyr::select(-seqnames, -start, -end, -strand) %>%
+    inner_join(classified_seq_tibble) %>%
+    filter(orf_start <= flank) %>%
+    filter(orf_end > width - flank) %>%
+    select(-width)
+)
 
 # single 
-single_frame_classified_orf_ranges <- as_granges(classified_seq_tibble)
-single_frame_classified_orf_ranges <- single_frame_classified_orf_ranges[single_frame_classified_orf_ranges$name
-                                                                         %in% single_frame_classified_orfs$name]
-
+single_frame_classified_orf_ranges <- as_granges(single_frame_classified_orfs)
 single_frame_classified_orfs_nt <- getSeq(genome_seq, single_frame_classified_orf_ranges)
 names(single_frame_classified_orfs_nt) <- paste0(single_frame_classified_orf_ranges$name)
 
-classes <- base::unique(single_frame_classified_ranges$class)
+classes <- base::unique(single_frame_classified_orf_ranges$class)
+
+compiled_aa <- readAAStringSet(paste0("out/classified_tnps/", species_name, "_compiled.fasta"))
 
 for(i in 1:length(classes)){
-
-  tmp <- readAAStringSet(paste0("out/classified_tnps/", classes[i], "/Laticauda_colubrina_", classes[i], ".fasta"))
-  tmp_nt <- single_frame_classified_orfs_nt[paste0(single_frame_classified_orf_ranges$name, "#", classes[i]) %in% names(tmp)]
+  
+  tmp_aa <- compiled_aa[names(compiled_aa) %in% paste0(single_frame_classified_orf_ranges$name, "#", classes[i])]
+  tmp_nt <- single_frame_classified_orfs_nt[single_frame_classified_orf_ranges$class == classes[i]]
+  
   if(length(tmp_nt) > 0){
     names(tmp_nt) <- paste0(names(tmp_nt), "#", classes[i])
-    writeXStringSet(tmp_nt, paste0("out/classified_tnps/", classes[i], "/Laticauda_colubrina_", classes[i], "_nt.fasta"))
+    writeXStringSet(tmp_aa, paste0("out/orfs/aa/", species_name, "_", classes[i], ".fasta"))
+    writeXStringSet(tmp_nt, paste0("out/orfs/nt/", species_name, "_", classes[i], ".fasta"))
   }
   
 }
 
-clstr <- read.csv("out/classified_tnps/Tc1marPlm/Laticauda_colubrina_Tc1marPlm_nt_cd-hit-est.fasta.clstr", sep = "\t",
-         row.names = NULL, header = FALSE, stringsAsFactors = FALSE) %>% as_tibble()
-
-clstr2 <- clstr
-n = nrow(clstr)
-x = 0
-numbers_only <- function(x) !grepl("\\D", x)
-for (row in c(1:n)) {
-  if (numbers_only(clstr2[row,1]) == TRUE) {
-    clstr2[row,1] <- x}
-  else {NULL}
-  x <- clstr2[row,1]
-}
-
-clstr2$V1 <- sub(">", "", clstr2$V1)
+# clstr <- read.csv("out/classified_tnps/Tc1marPlm/Laticauda_colubrina_Tc1marPlm_nt_cd-hit-est.fasta.clstr", sep = "\t",
+#          row.names = NULL, header = FALSE, stringsAsFactors = FALSE) %>% as_tibble()
+# 
+# clstr2 <- clstr
+# n = nrow(clstr)
+# x = 0
+# numbers_only <- function(x) !grepl("\\D", x)
+# for (row in c(1:n)) {
+#   if (numbers_only(clstr2[row,1]) == TRUE) {
+#     clstr2[row,1] <- x}
+#   else {NULL}
+#   x <- clstr2[row,1]
+# }
+# 
+# clstr2$V1 <- sub(">", "", clstr2$V1)
