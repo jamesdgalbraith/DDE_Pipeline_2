5a46d74a35e8057c08e731e01678f52d6d2dfba5
jamesdgalbraith <galbraithenator@gmail.com>
2021-12-01 17:18:08 +0000

    Created script for installing necessary packages

--- package_installer.R
+++ package_installer.R
@@ -6,5 +6,15 @@ if(!"future.apply" %in% installed.packages()){install.packages("future.apply")}
 if(!"openxlsx" %in% installed.packages()){install.packages("openxlsx")}
 if(!"BiocManager" %in% installed.packages()){install.packages("BiocManager")}
 if(!"BSgenome" %in% installed.packages()){BiocManager::install("BSgenome")}
+if(!"ORFik" %in% installed.packages()){BiocManager::install("ORFik")}
 if(!"plyranges" %in% installed.packages()){BiocManager::install("plyranges")}
 if(!"DECIPHER" %in% installed.packages()){BiocManager::install("DECIPHER")}
+
+library(tidyverse)
+
+fastas <- read_tsv("fastas.txt", col_names = "fasta")
+
+completed <- read_tsv("completed_genomes.txt", col_names = c("fasta", "species"))
+
+completed[!completed$fasta %in% fastas$fasta,]
+
